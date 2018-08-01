417244489604f5f3069c8258c85b58c215a4c32d
Filip Dej local <dejuniu@pctotemagh1.dyndns.cern.ch>
2018-08-01 12:32:06 +0200

    added plotting feature, fixed bugs

--- to_cal_json.py
+++ to_cal_json.py
@@ -1,6 +1,9 @@
 import json
 from io import open as io_open
 import argparse
+import re
+import matplotlib.pyplot as plt
+import numpy as np
 
 import ROOT
 from array import array
@@ -10,6 +13,7 @@ class FileParseException(Exception):
     pass
 
 def float_convertable(value):
+    """Checks if value is converable to float"""
     try:
         float(value)
         return True
@@ -42,6 +46,11 @@ def parse_calibration_file(file_name):
                 else:
                     temp_cells.append(parse_line(line))
                     if(end_of_channel(line_counter)):
+                        try:
+                            db = re.search('_(\d+)',file_name)
+                            temp_val["db"] = int(db.group(1))
+                        except(AttributeError):
+                            print("DB not specified in file name")
                         temp_val["sampic"] = 1 - ((line_counter - 1) // CELLS_NO) // CHANELS_NO
                         temp_val["chanel"] = ((line_counter - 1) // CELLS_NO) % 16
                         temp_val["cells"] = temp_cells
@@ -59,56 +68,89 @@ def process_cell(voltage, cell, fit):
     x = array('d',sorted(voltage))
     y = array('d',sorted(cell))
     graph = ROOT.TGraph(len(x),x,y)
-    parameters = graph.Fit(fit, 'S')
+    parameters = graph.Fit(fit, 'SQR')
     output = []
-    for i in range(0,2):
+    for i in range(0,fit.GetNpar()):
         output.append(parameters.Value(i))
     return output
 
 def handle_optional_parameters(data, file_name, output_file_name, fit_from, fit_to):
     """Sets default values of optional parameters if unset"""
-    if(fit_from is None or fit_from < 0):
+    if((fit_from is None) or (float(fit_from) < 0)):
         fit_from=0
 
-    max_fit_to = len(data["voltage"])
-    if(fit_to is None or fit_to > max_fit_to):
+    max_fit_to = max(data["voltage"])
+    if((fit_to is None) or (float(fit_to) > max_fit_to)):
         fit_to=max_fit_to
 
     if(output_file_name is None):
         output_file_name = file_name[:file_name.rfind('.')] + '.cal.json' # If no other filename specified save in the same location as input file
     else:
-        output_file_name += '.cal.json'
-    return output_file_name, fit_from, fit_to
+        if output_file_name[-1] != "/":
+            output_file_name += "/"
+        output_file_name += file_name[file_name.rfind('/')+1:file_name.rfind('.')] + '.cal.json'
+    return output_file_name, float(fit_from), float(fit_to)
+
+def draw_plots(x, all_y, fit, chanel_to_plot):
+    """Draws plots"""
+    plt.figure(1)
+    plt.title("Chanel "+ chanel_to_plot + " cells")
+    plt.xlabel("Voltage")
+    plt.ylabel("Digital value")
+    for y in all_y:
+        plt.plot(x,y)
+    process_cell(x, all_y[0], fit)
+    fit_val = [fit.Eval(i) for i in x]
+    residual_val = [abs(i-j) for i,j in zip(fit_val,all_y[0])]
+    fig = plt.figure(2)
+    plt.title("Values and Fit")
+    plt.xlabel("Voltage")
+    plt.ylabel("Digital value")
+    plt.plot(x,all_y[0])
+    plt.plot(x,fit_val)
+    plt.legend(["Digital values","Fitted function"],bbox_to_anchor=(1, 1), loc=4, borderaxespad=0.)
+    plt.figure(3)
+    plt.title("Fit residual")
+    plt.xlabel("Voltage")
+    plt.ylabel("Residual")
+    plt.scatter(x,residual_val)
+    plt.show()
 
 
-def convert_calibration_file(file_name, function, output_file_name=None, fit_from=None, fit_to=None):
-    """ """
+def convert_calibration_file(file_name, function, output_file_name=None, fit_from=None, fit_to=None, chanel_to_plot=None):
+    """Main calibration function"""
     data = parse_calibration_file(file_name)
     output_file_name, fit_from, fit_to = handle_optional_parameters(data, file_name, output_file_name, fit_from, fit_to)
 
     fit = ROOT.TF1("fit",function, fit_from, fit_to)
+
+    if(chanel_to_plot is not None):
+        chanel_no = chanel_to_plot
+        draw_plots(data["voltage"], data["values"][int(chanel_no)]["cells"], fit, chanel_no)
+
     all_values = data["values"]
     for value in all_values:
         conv_value = []
         for cell in value["cells"]:
             conv_value.append(process_cell(data["voltage"], cell, fit))
         value["cells"] = conv_value
-
     del data["voltage"]
-    data["function"] = function
+    data["parameters"] = data.pop("values")
+    data["formula"] = function
 
     with open(output_file_name, 'w') as outfile:
         json.dump(data, outfile)
 
 def main():
     parser = argparse.ArgumentParser(description='Converts text calibration data to .cal.JSON format,by fitting function to the given data and storing its parameters.')
-    parser.add_argument("input_file",action="store",help="Input file name")
-    parser.add_argument("function",action="store",help="Function to fit i.e. \"[0]*x+[1]\"")
-    parser.add_argument("-o", "--output", action="store", help="Path to the location where output will be stored")
-    parser.add_argument("-f", "--fit_from", action="store", help="Left bound of fit values interval")
-    parser.add_argument("-t", "--fit_to", action="store", help="Right bound of fit values interval")
+    parser.add_argument("input_file",action="store",help="set input file name")
+    parser.add_argument("function",action="store",help="set function to fit i.e. \'[0]*x+[1]\'")
+    parser.add_argument("-o", "--output", action="store", metavar= "PATH/TO/FILE", help="set path where file schould be stored")
+    parser.add_argument("-f", "--fit_from", action="store", metavar= "LEFT_BOUND", help="set left bound of fit values interval")
+    parser.add_argument("-t", "--fit_to", action="store", metavar= "RIGHT_BOUND", help="set right bound of fit values interval")
+    parser.add_argument("-p", "--plot", action="store", metavar= "CHANEL_NO", help="enable plotting mode, takes number of channel to plot")
     args = parser.parse_args()
-    convert_calibration_file(args.input_file, args.function, args.output, args.fit_from, args.fit_to)
+    convert_calibration_file(args.input_file, args.function, args.output, args.fit_from, args.fit_to, args.plot)
 
 if __name__ == '__main__':
     main()
