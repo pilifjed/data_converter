36edaea6bea357d2241863fd3b77dbcc445d0220
Filip <pilifjed@gmail.com>
2018-08-02 16:55:51 +0200

    Added support for multiple files and merging functionality

--- to_cal_json.py
+++ to_cal_json.py
@@ -12,6 +12,9 @@ from array import array
 class FileParseException(Exception):
     pass
 
+class FileMergeException(Exception):
+    pass
+
 def float_convertable(value):
     """Checks if value is converable to float"""
     try:
@@ -36,8 +39,16 @@ def parse_calibration_file(file_name):
         first = True
         line_counter = 1
         temp_cells = []
-        output = {"values":[]}
-        temp_val = {"db": -1, "sampic": -1, "chanel": -1, "cells": []}
+        output = {"values":{}}
+        temp_val = {}
+
+        try:
+            db = re.search('_db(\d+)',file_name)
+            temp_val["db"] = int(db.group(1))
+        except AttributeError:
+            raise FileParseException("DB not specified in file name")
+
+        output["values"][temp_val["db"]] = []
         for line in text_file:
             if(float_convertable(line.split(" ")[0])):
                 if(first):
@@ -46,21 +57,17 @@ def parse_calibration_file(file_name):
                 else:
                     temp_cells.append(parse_line(line))
                     if(end_of_channel(line_counter)):
-                        try:
-                            db = re.search('_(\d+)',file_name)
-                            temp_val["db"] = int(db.group(1))
-                        except(AttributeError):
-                            print("DB not specified in file name")
+                        temp_val["time_offset"] = 0
                         temp_val["sampic"] = 1 - ((line_counter - 1) // CELLS_NO) // CHANELS_NO
                         temp_val["chanel"] = ((line_counter - 1) // CELLS_NO) % 16
                         temp_val["cells"] = temp_cells
-                        output["values"].append(temp_val.copy())
+                        output["values"][temp_val["db"]].append(temp_val.copy())
                         temp_cells = []
                     line_counter+=1
 
-        if(VALUES_NO != len(output["values"])):
+        if(VALUES_NO != len(output["values"][next(iter(output["values"]))])):
             raise FileParseException('Corrupt file or wrong CHANELS_NO, SAMPIC_NO or CELLS_NO values specified. Expected CHANELS_NO*SAMPIC_NO = '
-                                     + str(VALUES_NO) +', got '+ str(len(output["values"])))
+                                     + str(VALUES_NO) +', got '+ str(len(output["values"][next(iter(output["values"]))])))
     return output
 
 def process_cell(voltage, cell, fit):
@@ -74,22 +81,16 @@ def process_cell(voltage, cell, fit):
         output.append(parameters.Value(i))
     return output
 
-def handle_optional_parameters(data, file_name, output_file_name, fit_from, fit_to):
+def handle_optional_parameters(parsed_data, fit_from, fit_to):
     """Sets default values of optional parameters if unset"""
-    if((fit_from is None) or (float(fit_from) < 0)):
+    if((fit_from is None)): # """or (float(fit_from) < 0)"""
         fit_from=0
-
-    max_fit_to = max(data["voltage"])
-    if((fit_to is None) or (float(fit_to) > max_fit_to)):
+    if((fit_to is None)): # """or (float(fit_to) > max_fit_to)"""
+        max_fit_to = min(max((float(x) for x in data["voltage"])) for file_name, data in parsed_data)
         fit_to=max_fit_to
+    return  float(fit_from), float(fit_to)
 
-    if(output_file_name is None):
-        output_file_name = file_name[:file_name.rfind('.')] + '.cal.json' # If no other filename specified save in the same location as input file
-    else:
-        if output_file_name[-1] != "/":
-            output_file_name += "/"
-        output_file_name += file_name[file_name.rfind('/')+1:file_name.rfind('.')] + '.cal.json'
-    return output_file_name, float(fit_from), float(fit_to)
+
 
 def draw_plots(x, all_y, fit, chanel_to_plot):
     """Draws plots"""
@@ -117,18 +118,18 @@ def draw_plots(x, all_y, fit, chanel_to_plot):
     plt.show()
 
 
-def convert_calibration_file(file_name, function, output_file_name=None, fit_from=None, fit_to=None, chanel_to_plot=None):
+def convert_calibration_file(data, function, fit_from=None, fit_to=None, chanel_to_plot=None):
     """Main calibration function"""
-    data = parse_calibration_file(file_name)
-    output_file_name, fit_from, fit_to = handle_optional_parameters(data, file_name, output_file_name, fit_from, fit_to)
 
     fit = ROOT.TF1("fit",function, fit_from, fit_to)
 
+    db_no = next(iter(data["values"]))
+
     if(chanel_to_plot is not None):
         chanel_no = chanel_to_plot
-        draw_plots(data["voltage"], data["values"][int(chanel_no)]["cells"], fit, chanel_no)
+        draw_plots(data["voltage"], data["values"][db_no][int(chanel_no)]["cells"], fit, chanel_no)
 
-    all_values = data["values"]
+    all_values = data["values"][db_no]
     for value in all_values:
         conv_value = []
         for cell in value["cells"]:
@@ -137,20 +138,84 @@ def convert_calibration_file(file_name, function, output_file_name=None, fit_fro
     del data["voltage"]
     data["parameters"] = data.pop("values")
     data["formula"] = function
+    return data
 
-    with open(output_file_name, 'w') as outfile:
-        json.dump(data, outfile)
+def read_cal_json(file_name):
+    with open(file_name) as file:
+        data = json.load(file)
+    return data
+
+def merge(to_merge):
+    merged_data = to_merge.pop()
+    merged_keys = list(merged_data["parameters"].keys())
+    for data in to_merge:
+        for key in data["parameters"]:
+            if key not in merged_keys:
+                merged_keys.append(key)
+                merged_data["parameters"][key] = data["parameters"][key]
+            else:
+                raise FileMergeException('Trying to merge files with the same digitizer board')
+    return merged_data
+
+def has_extension(extension, file_name):
+    return re.search(extension, file_name, re.IGNORECASE) is not None
 
 def main():
-    parser = argparse.ArgumentParser(description='Converts text calibration data to .cal.JSON format,by fitting function to the given data and storing its parameters.')
-    parser.add_argument("input_file",action="store",help="set input file name")
-    parser.add_argument("function",action="store",help="set function to fit i.e. \'[0]*x+[1]\'")
-    parser.add_argument("-o", "--output", action="store", metavar= "PATH/TO/FILE", help="set path where file schould be stored")
-    parser.add_argument("-f", "--fit_from", action="store", metavar= "LEFT_BOUND", help="set left bound of fit values interval")
-    parser.add_argument("-t", "--fit_to", action="store", metavar= "RIGHT_BOUND", help="set right bound of fit values interval")
-    parser.add_argument("-p", "--plot", action="store", metavar= "CHANEL_NO", help="enable plotting mode, takes number of channel to plot")
+    parser = argparse.ArgumentParser(description='Converts text calibration data to .cal.json format,by fitting function to the given data and storing its parameters.')
+    parser.add_argument("input_files", action="store", nargs="+", help="set input file names")
+    parser.add_argument("-f", "--function", default="pol1",action="store",help="set function to fit i.e. \'[0]*x+[1]\'")
+    parser.add_argument("-l", "--fit_from", action="store", metavar="LEFT_FIT_BOUND", help="set left bound of fit values interval")
+    parser.add_argument("-r", "--fit_to", action="store", metavar="RIGHT_FIT_BOUND", help="set right bound of fit values interval")
+    parser.add_argument("-p", "--plot",action="store", metavar="CHANEL_TO_PLOT_NO", help="enable plotting during conversion")
+    parser.add_argument("-m", "--merge", action="store", metavar="OUTPUT_FILE_NAME", help="enable merge mode")
     args = parser.parse_args()
-    convert_calibration_file(args.input_file, args.function, args.output, args.fit_from, args.fit_to, args.plot)
+
+    parsed_data = []
+    converted_data = []
+    output_data = []
+
+
+
+    for file_name in args.input_files:
+        if has_extension(".cal.json", file_name):
+            data = read_cal_json(file_name)
+            function = data["formula"]
+            if args.merge and function != args.function:
+                print(file_name + "didn't match function specified for conversion, file won't be included in merged output file")
+            else:
+                output_data.append((file_name, data))
+        else:
+            try:
+                data = parse_calibration_file(file_name)
+                parsed_data.append((file_name, data))
+            except Exception as e:
+                print("File " + file_name + " skipped. An error ocurred during parse process: " + str(e))
+
+    if parsed_data != []:
+        fit_from, fit_to = handle_optional_parameters(parsed_data, args.fit_from, args.fit_to)
+
+    for file_name, data in parsed_data:
+        try:
+            new_data = convert_calibration_file(data, args.function, fit_from, fit_to, args.plot)
+            converted_data.append((file_name, new_data))
+            args.plot = None # only first file will be plotted
+        except Exception as e:
+            print("File " + file_name + " skipped. An error ocurred during conversion process: " + str(e))
+
+    for file_name, data in converted_data:
+        output_file_name = file_name[:file_name.rfind(".")] + ".cal.json"
+        output_data.append((output_file_name, data))
+
+    if args.merge is not None:
+        merge_file_name = args.merge
+        if not has_extension(".cal.json", args.merge):
+            merge_file_name += ".cal.json"
+        merged_data = merge([data for file_name, data in output_data])
+        output_data = [(merge_file_name, merged_data)]
+
+    for output_file_name, data in output_data:
+        with open(output_file_name, 'w') as outfile:
+            json.dump(data, outfile)
 
 if __name__ == '__main__':
     main()
